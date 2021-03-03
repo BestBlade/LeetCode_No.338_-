3680f6bd0c7b1ff5cf9a93d56f384a1e1212a4f4
BestBlade <68151785+BestBlade@users.noreply.github.com>
2021-03-03 09:36:02 +0800

    Add files via upload

---
+++ leetcode_no338_比特位计数_w1.cpp
@@ -0,0 +1,83 @@
+﻿/* ------------------------------------------------------------------|
+给定一个非负整数 num。对于 0 ≤ i ≤ num 范围中的每个数字 i ，计算其二进制数中的 1 的数目并将它们作为数组返回。
+
+示例 1:
+
+输入: 2
+输出: [0,1,1]
+示例 2:
+
+输入: 5
+输出: [0,1,1,2,1,2]
+进阶:
+
+给出时间复杂度为O(n*sizeof(integer))的解答非常容易。但你可以在线性时间O(n)内用一趟扫描做到吗？
+要求算法的空间复杂度为O(n)。
+你能进一步完善解法吗？要求在C++或任何其他语言中不使用任何内置函数（如 C++ 中的 __builtin_popcount）来执行此操作。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/counting-bits
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。|
+-------------------------------------------------------------------*/
+
+/*
+*
+*	执行用时：4 ms, 在所有 C++ 提交中击败了96.36%的用户
+*	内存消耗：7.5 MB, 在所有 C++ 提交中击败了95.20%的用户
+*/
+
+#include <iostream>
+#include <string> 
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <map>
+#include <stack>
+#include <queue>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode(int x) : val(x), next(NULL) {}
+
+};
+
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+
+vector<int> countBits(int num) {
+    vector<int> dp(num + 1);
+    int prev = 1;
+    for (int i = 1; i <= num; i++) {
+        if (i == (prev << 1)) {
+            prev = i;
+        }
+        dp[i] = dp[i - prev] + 1;
+    }
+    return dp;
+}
+
+int main() {
+    countBits(2);
+}
